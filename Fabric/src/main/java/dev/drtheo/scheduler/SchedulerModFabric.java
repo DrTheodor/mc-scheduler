b8d0eab6dcaacb87843d987a72b77579efea623e
James Hall <james.hall01@outlook.com>
2025-02-26 18:59:18 +0000

    chore: bring over api scheduler code chore: dev-env only tests

--- Fabric/src/main/java/dev/drtheo/scheduler/SchedulerModFabric.java
+++ Fabric/src/main/java/dev/drtheo/scheduler/SchedulerModFabric.java
@@ -1,11 +1,29 @@
 package dev.drtheo.scheduler;
 
+import dev.drtheo.scheduler.client.SchedulerModClientCommon;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class SchedulerModFabric implements ModInitializer {
+
+	@Environment(EnvType.CLIENT)
+	private static void setupTicking() {
+		ClientTickEvents.END_CLIENT_TICK.register(client -> SchedulerModClientCommon.tick());
+	}
 
 	@Override
 	public void onInitialize() {
 		SchedulerModCommon.init();
+
+		if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+			SchedulerModClientCommon.init();
+			setupTicking();
+		}
+
+		ServerTickEvents.END_SERVER_TICK.register(server -> SchedulerModCommon.tick());
 	}
 }
--- Forge/src/main/java/dev/drtheo/scheduler/SchedulerModForge.java
+++ Forge/src/main/java/dev/drtheo/scheduler/SchedulerModForge.java
@@ -1,11 +1,28 @@
 package dev.drtheo.scheduler;
 
+import dev.drtheo.scheduler.client.SchedulerModClientCommon;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLLoader;
 
 @Mod(Constants.MOD_ID)
+@Mod.EventBusSubscriber(modid = Constants.MOD_ID)
 public class SchedulerModForge {
 
 	public SchedulerModForge() {
 		SchedulerModCommon.init();
+
+		if (FMLLoader.getDist() == Dist.CLIENT) {
+			SchedulerModClientCommon.init();
+		}
+	}
+
+	@SubscribeEvent
+	public static void onServerTick(TickEvent.ServerTickEvent e) {
+		if (e.phase == TickEvent.Phase.END) {
+			SchedulerModCommon.tick();
+		}
 	}
 }
